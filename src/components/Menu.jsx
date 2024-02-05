e9a8910fec69f19fa8740b48fa02638328365037
illu_y <ii_2002@naver.com>
2024-02-05 12:26:25 +0900

    메뉴 변경

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Menu = () => {
     return (
         <nav>
-            <li>1번</li>
-            <li>2번</li>
-            <li>3번</li>
-            <li>4번</li>
+            <li>Karina</li>
+            <li>Winter</li>
+            <li>Giselle</li>
+            <li>Ningning</li>
         </nav>
     );
 };
