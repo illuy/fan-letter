6cbd9d752474ef71858781057972cd6416f0d1ca
illu_y <ii_2002@naver.com>
2024-02-06 10:27:19 +0900

    styled.components 변경 + 메뉴활성화시 변경

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,35 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const Menu = ({ setSelectedTab }) => {
+const TabList = ({ tabs, handleTabClick, activeTab }) => (
+    <MenuWrapBox>
+        {tabs.map((tab) => (
+            <TabItem
+                key={tab}
+                tab={tab}
+                handleTabClick={handleTabClick}
+                isActive={activeTab === tab}
+            />
+        ))}
+    </MenuWrapBox>
+);
+
+const Menu = () => {
+    const [activeTab, setActiveTab] = useState("Karina");
+
     const handleTabClick = (tab) => {
-        setSelectedTab(tab);
+        setActiveTab(tab);
     };
+
+    const tabs = ["Karina", "Winter", "Giselle", "Ningning"];
+
     return (
-        <nav>
-            <ul>
-                <li>
-                    <button onClick={() => handleTabClick("Karina")}>
-                        Karina
-                    </button>
-                </li>
-                <li>
-                    <button onClick={() => handleTabClick("Winter")}>
-                        Winter
-                    </button>
-                </li>
-                <li>
-                    <button onClick={() => handleTabClick("Giselle")}>
-                        Giselle
-                    </button>
-                </li>
-                <li>
-                    <button onClick={() => handleTabClick("Ningning")}>
-                        Ningning
-                    </button>
-                </li>
-            </ul>
-        </nav>
+        <MenuWrap>
+            <TabList
+                tabs={tabs}
+                handleTabClick={handleTabClick}
+                activeTab={activeTab}
+            />
+        </MenuWrap>
     );
 };
+const TabItem = ({ tab, handleTabClick, isActive }) => (
+    <MenuWrapItem className={isActive ? "active" : ""}>
+        <MenuWrapBtn onClick={() => handleTabClick(tab)}>{tab}</MenuWrapBtn>
+    </MenuWrapItem>
+);
 
 export default Menu;
+
+const MenuWrap = styled.nav`
+    display: flex;
+    justify-content: center;
+    margin: 2rem;
+`;
+const MenuWrapBox = styled.ul`
+    display: flex;
+    justify-content: center;
+    background-color: #212121;
+    border-radius: 0.5rem;
+    padding: 1rem;
+`;
+
+const MenuWrapItem = styled.li`
+    margin-left: 1rem;
+    color: #fff;
+    &:nth-child(1) {
+        margin-left: 0rem;
+    }
+    &.active {
+        color: orange;
+    }
+`;
+
+const MenuWrapBtn = styled.button`
+    background-color: transparent;
+    border: 0;
+    color: inherit;
+    cursor: pointer;
+    &:hover {
+        color: orange;
+    }
+`;
