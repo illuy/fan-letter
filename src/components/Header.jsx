6fe119955a07a87b7d1f1497192ff3ce92221d64
illu_y <ii_2002@naver.com>
2024-02-06 10:26:47 +0900

    styled 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
     return (
-        <header>
+        <HeaderWrap>
             <div>Login</div>
-            <h1>
+            <Logo>
                 <a href="./">LOGO</a>
-            </h1>
+            </Logo>
             <div>Menu Icon</div>
-        </header>
+        </HeaderWrap>
     );
 };
 
 export default Header;
+
+const HeaderWrap = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    background-color: #212121;
+    color: #fff;
+`;
+const Logo = styled.h1`
+    font-size: 2rem;
+`;
